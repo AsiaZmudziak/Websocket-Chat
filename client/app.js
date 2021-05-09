2cc4b0a92dcf15de3dbb5e0cda56ec99289457d3
unknown <asiazmudziak@gmail.com>
2021-05-09 23:22:21 +0200

    add HTML structure&styles

---
+++ client/app.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById('welcome-form'); //reference to login form
+const messagesSection = document.getElementById('messages-section'); //reference to messages section
+const messagesList = document.getElementById('messages-list'); //reference to messages list
+const addMessageForm = document.getElementById('add-messages-form'); //reference to add message form
+const userNameInput = document.getElementById('username'); //reference to text field from the login form
+const messageContentInput = document.getElementById('message-content'); //reference to text field of the form for sending a message
+
+let userName = ''; 
+
